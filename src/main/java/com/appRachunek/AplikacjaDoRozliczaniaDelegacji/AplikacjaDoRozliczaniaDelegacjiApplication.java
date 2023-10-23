ea05224b9352fbbe3926e0d44ab6dbfdc9e4639c
Jakub Rutkowski <kubol0075@gmail.com>
2023-10-23 09:56:34 +0200

    Init AplikacjaDoRozliczaniaDelegacji projekt

---
+++ src/main/java/com/appRachunek/AplikacjaDoRozliczaniaDelegacji/AplikacjaDoRozliczaniaDelegacjiApplication.java
@@ -0,0 +1,13 @@
+package com.appRachunek.AplikacjaDoRozliczaniaDelegacji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikacjaDoRozliczaniaDelegacjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikacjaDoRozliczaniaDelegacjiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appRachunek/AplikacjaDoRozliczaniaDelegacji/AplikacjaDoRozliczaniaDelegacjiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appRachunek.AplikacjaDoRozliczaniaDelegacji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikacjaDoRozliczaniaDelegacjiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
